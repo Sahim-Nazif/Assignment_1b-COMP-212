c5ade64ee822858899915bcd2f633849a691c194
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-01-16 20:01:05 -0500

    Add project files.

---
+++ Assignment_1b/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assignment_1b
+{
+    //declaring delegate
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
