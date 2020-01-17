1ca2d304b30af80f91d6a3c737c72042e5c21e1c
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-01-16 20:10:53 -0500

    created delegate and method to keep track of sold cars

--- Assignment_1b/Program.cs
+++ Assignment_1b/Program.cs
@@ -3,11 +3,35 @@
 namespace Assignment_1b
 {
     //declaring delegate
+    public delegate void CarInventory();
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("\nWelcome To COMP 212 INVENTORY EVENT PROGRAM OF NAZIF SAHIM");
+
+
+        }
+
+        public static void CarSold()
+        {
+            Console.WriteLine("\nHow many cars are in Stock currently  ? " + " ");
+            int car = int.Parse(Console.ReadLine());
+            for (int i=0; i<car; i++)
+            {
+                Console.WriteLine("\nHow many cars did you sell ? " + " ");
+                int sold = int.Parse(Console.ReadLine());
+
+                if (sold>car)
+                {
+                    Console.WriteLine("Error.. Please check ! You sold more than inventory ?");
+                }
+                else
+                {
+                    car = car - sold;
+                }
+            }
         }
     }
 }
